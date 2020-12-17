1063b05f94f00a4cd1d8b812e77db41f3e785e15
MohamadEslim <mohammadeslim@gmail.com>
2020-12-17 13:25:50 +0200

    transaction fixes

--- src/controllers/transaction_controller.js
+++ src/controllers/transaction_controller.js
@@ -61,6 +61,7 @@ module.exports = {
                     if(!entryTransaction){
                         return res.json("car_notFounded")
                     }
+                    
                     console.log(entryTransaction.transaction_time)
                     console.log(transaction.transaction_time)
                     var difference = transaction.transaction_time - entryTransaction.transaction_time;
